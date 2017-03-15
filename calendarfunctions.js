ec494f03c2c9490d3d1cf90bfef9d5fd5b924c95
Austin Prince <amprince@wustl.edu>
2017-03-14 22:14:13 -0400

    calendar view is working css needs to be updated

--- calendarfunctions.js
+++ calendarfunctions.js
@@ -1,74 +1,131 @@
-(function () {
-	"use strict";
+var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-	Date.prototype.deltaDays = function (n) {
-		// relies on the Date object to automatically wrap between months for us
-		return new Date(this.getFullYear(), this.getMonth(), this.getDate() + n);
-	};
+(function() {
+    Date.prototype.deltaDays = function(c) {
+        return new Date(this.getFullYear(), this.getMonth(), this.getDate() + c)
+    };
+    Date.prototype.getSunday = function() {
+        return this.deltaDays(-1 * this.getDay())
+    }
+})();
 
-	Date.prototype.getSunday = function () {
-		return this.deltaDays(-1 * this.getDay());
-	};
-}());
-
-
-function Week(initial_d) {
-	"use strict";
-
-	this.sunday = initial_d.getSunday();
-
-
-	this.nextWeek = function () {
-		return new Week(this.sunday.deltaDays(7));
-	};
-
-	this.prevWeek = function () {
-		return new Week(this.sunday.deltaDays(-7));
-	};
-
-	this.contains = function (d) {
-		return (this.sunday.valueOf() === d.getSunday().valueOf());
-	};
-
-	this.getDates = function () {
-		var dates = [];
-		for(var i=0; i<7; i++){
-			dates.push(this.sunday.deltaDays(i));
-		}
-		return dates;
-	};
+function Week(c) {
+    this.sunday = c.getSunday();
+    this.nextWeek = function() {
+        return new Week(this.sunday.deltaDays(7))
+    };
+    this.prevWeek = function() {
+        return new Week(this.sunday.deltaDays(-7))
+    };
+    this.contains = function(b) {
+        return this.sunday.valueOf() === b.getSunday().valueOf()
+    };
+    this.getDates = function() {
+        for (var b = [], a = 0; 7 > a; a++) b.push(this.sunday.deltaDays(a));
+        return b
+    }
 }
 
-function Month(year, month) {
-	"use strict";
+function Month(c, b) {
+    this.year = c;
+    this.month = b;
+    this.nextMonth = function() {
+        return new Month(c + Math.floor((b + 1) / 12), (b + 1) % 12)
+    };
+    this.prevMonth = function() {
+        return new Month(c + Math.floor((b - 1) / 12), (b + 11) % 12)
+    };
+    this.getDateObject = function(a) {
+        return new Date(this.year, this.month, a)
+    };
+    this.getWeeks = function() {
+        var a = this.getDateObject(1),
+            b = this.nextMonth().getDateObject(0),
+            c = [],
+            a = new Week(a);
+        for (c.push(a); !a.contains(b);) a = a.nextWeek(), c.push(a);
+        return c
+    }
+};
 
-	this.year = year;
-	this.month = month;
+var currentDate = new Date();
+var month = currentDate.getMonth();
+var currentYear = currentDate.getFullYear();
+// For our purposes, we can keep the current month in a variable in the global scope
+var currentMonth = new Month(currentYear, month); // currentMonth
+var tempMonth = month+1;
+var tempDate = new Date(tempMonth +' 1 ,'+currentYear);
+var tempweekday= tempDate.getDay();
+var tempweekday2 = tempweekday;
+console.log(tempDate);
 
-	this.nextMonth = function () {
-		return new Month( year + Math.floor((month+1)/12), (month+1) % 12);
-	};
 
-	this.prevMonth = function () {
-		return new Month( year + Math.floor((month-1)/12), (month+11) % 12);
-	};
 
-	this.getDateObject = function(d) {
-		return new Date(this.year, this.month, d);
-	};
+// Change the month when the "next" button is pressed
+document.getElementById("next_month_btn").addEventListener("click", function(event){
+	currentMonth = currentMonth.nextMonth(); // Previous month would be currentMonth.prevMonth()
+	// if (currentMonth.month == 11) {
+	// 	currentYear+=1;
+	// }
+	updateCalendar(); // Whenever the month is updated, we'll need to re-render the calendar in HTML
+	alert("The new month is "+currentMonth.month+" "+currentMonth.year);
 
-	this.getWeeks = function () {
-		var firstDay = this.getDateObject(1);
-		var lastDay = this.nextMonth().getDateObject(0);
+}, false);
 
-		var weeks = [];
-		var currweek = new Week(firstDay);
-		weeks.push(currweek);
-		while(!currweek.contains(lastDay)){
-			currweek = currweek.nextWeek();
-			weeks.push(currweek);
+document.getElementById("prev_month_btn").addEventListener("click", function(event){
+	currentMonth = currentMonth.prevMonth(); // Previous month would be currentMonth.prevMonth()
+	// if (currentMonth.month == 0) {
+	// 	currentYear-=1;
+	// }
+	updateCalendar(); // Whenever the month is updated, we'll need to re-render the calendar in HTML
+	alert("The new month is "+currentMonth.month+" "+currentMonth.year);
+}, false);
+
+
+// This updateCalendar() function only alerts the dates in the currently specified month.  You need to write
+// it to modify the DOM (optionally using jQuery) to display the days and weeks in the current month.
+function updateCalendar(){
+
+	clearCalendar()
+	var weeks = currentMonth.getWeeks();
+	document.getElementById('month_display').innerHTML = monthNames[currentMonth.month] + " " + currentYear;
+	for(var w in weeks){
+		var days = weeks[w].getDates();
+		var weekrow = document.createElement("tr");
+
+		//alert("Week starting on "+days[0]);
+
+		for(var d in days){
+			if (days[d].getMonth() != currentMonth.month) {
+				var tablecell = document.createElement("td");
+				tablecell.appendChild(document.createTextNode(""));
+				weekrow.appendChild(tablecell);
+				console.log("Not in current month");
+				console.log(days[d].getMonth());
+
+			}
+			// You can see console.log() output in your JavaScript debugging tool, like Firebug,
+			// WebWit Inspector, or Dragonfly.
+			else {
+				console.log(days[d]);
+
+				var tablecell = document.createElement("td");
+				tablecell.appendChild(document.createTextNode(days[d].getDate()));
+				tablecell.setAttribute("id", days[d]);
+				weekrow.appendChild(tablecell);
+
+
 		}
+		document.getElementById('calendarmain').appendChild(weekrow);
+		}
+	}
+}
 
-		return weeks;
-	};
+function clearCalendar() {
+	var main = document.getElementById('calendarmain');
+	while (main.childNodes.length > 2) {
+    main.removeChild(main.lastChild);
+}
+
+
 }
--- loginAjax.js
+++ loginAjax.js
@@ -14,6 +14,9 @@ xmlHttp.addEventListener("load", function(event){
   var jsonData = JSON.parse(event.target.responseText); // parse the JSON into a JavaScript object
   if(jsonData.success){  // in PHP, this was the "success" key in the associative array; in JavaScript, it's the .success property of jsonData
     alert("You've been Logged In!");
+    //document.getElementById('loginuser').style.visibility="hidden";
+    document.getElementById('adduser').style.visibility="hidden";
+    document.getElementById('logout').style.visibility="visible";
   }else{
     alert("You were not logged in.  "+jsonData.message);
   }
@@ -34,18 +37,24 @@ function registerAjax(event) {
   xmlHttp.open("POST", "register.php", true);
   xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); // It's easy to forget this line for POST requests
   xmlHttp.addEventListener("load", function(event){
-    console.log("responseText");
-    console.log(event.target.responseText);
+    //console.log("responseText");
+    //console.log(event.target.responseText);
     var jsonData = JSON.parse(event.target.responseText);
     if (jsonData.success) {
+      console.log("trying to hide register");
       alert("Registration success");
+      document.getElementById('adduser').style.visibility="hidden";
     }
     else {
       alert("Registration Failed. "+jsonData.message);
     }
   }, false);
   xmlHttp.send(dataString);
-  document.getElementById('adduser').style.visibility="hidden";
+
 }
 
 document.getElementById('registernew').addEventListener('click', registerAjax, false);
+
+// function logoutAjax(event) {
+//   varX
+// }
